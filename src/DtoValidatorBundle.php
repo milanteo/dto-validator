ef5791a3f558cd7c99c4bd9119e5d59830c8884a
milanteo <milanteo@live.it>
2024-12-08 15:42:16 +0000

    yeah

--- src/DtoValidatorBundle.php
+++ src/DtoValidatorBundle.php
@@ -2,6 +2,16 @@
 
 namespace Teoalboo\DtoValidator;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class DtoValidatorBundle extends AbstractBundle { }
+class DtoValidatorBundle extends AbstractBundle {
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.yaml');
+    }
+
+ }
